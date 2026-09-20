a637034091b86b958fd0b7265622b170d23828de
Oskars <oskars.pakers@gmail.com>
2026-09-20 11:36:53 +0300

    Show song tags as clickable chips above the song sheet (#69)

---
+++ src/components/SongTags.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { LxButton } from '@dativa-lv/lx-ui';
+
+defineProps({
+  tags: {
+    type: Array,
+    default: () => [],
+  },
+});
+
+function tagLink(tag) {
+  return {
+    name: 'tagView',
+    params: { url: tag.url.replace(/^\/tag\//, '') },
+  };
+}
+</script>
+<style scoped>
+.song-tags {
+  display: flex;
+  flex-wrap: wrap;
+  gap: var(--space-0500, 0.5rem);
+  margin-bottom: var(--space-1000, 1rem);
+}
+</style>
+<template>
+  <div class="song-tags">
+    <LxButton
+      v-for="tag in tags"
+      :key="tag.id"
+      kind="tertiary"
+      icon="tag"
+      :label="tag.title"
+      :href="tagLink(tag)"
+    />
+  </div>
+</template>
